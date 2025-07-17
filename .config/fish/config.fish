6fc70e19fa52d70cc69d4d532c39a443e5116e56
Pinkdino <pinkfluffydino@proton.me>
2025-07-16 20:25:45 -0500

    Add fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#greeting
+function fish_greeting
+    echo Hello (set_color red)$USER!(set_color normal)
+    echo Time : (set_color yellow)(date +%T)(set_color normal)
+end
+
+#perl use english locale 
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
+#aliases
+#ls
+alias ls="ls -lh"
+
+#starship
+starship init fish | source 
